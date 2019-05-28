c22579a3b7530c492c57143063b286d2f6727e27
amiao <353639110@qq.com>
2019-05-28 10:38:58 +0800

    修改跨域信息

--- src/globalConfig.vue
+++ src/globalConfig.vue
@@ -4,7 +4,7 @@
     const DEFAULT_OSS_URL_ITEM = "https://arknights.oss-cn-shanghai.aliyuncs.com/items/";
     const DEFAULT_OSS_SUFFIX = ".png";
     // api接口地址
-    const DEFAULT_API_URL = "http://47.101.154.8/api";
+    const DEFAULT_API_URL = "http://www.shuijiaomao.com/api";
     // const DEFAULT_API_URL = "http://127.0.0.1:9390";
     // 本地缓存token key
     const MIAOMIAO_TOKEN_PREFIX = "miaomiaoTokenPrefix";
